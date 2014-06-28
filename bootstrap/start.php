1b4eb743a8dd5a723dbf374849f7dec9094b1865
Alastair Paragas <alastairparagas@gmail.com>
2014-06-28 19:14:56 -0400

    Added AcerAspireS3391 computer as local environment

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('SamsungRF711'),
+	'local' => array('SamsungRF711','AcerAspireS3391'),
 
 ));
 
