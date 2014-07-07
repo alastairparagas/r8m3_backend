d978152a8bef1b1ee99d10fc8044cce44e8a16f3
Alastair Paragas <alastairparagas@gmail.com>
2014-07-07 01:22:03 -0400

    Renamed image_file to file column and set up model relationships

--- app/models/Image.php
+++ app/models/Image.php
@@ -12,13 +12,14 @@ class Image extends Ardent{
     public static $rules = array(
         'user_id' => 'required|numeric|exists:users,id',
         'id' => 'required|unique:images,id',
-        'image_file' => 'required|unique:images,image_file|mimes:jpeg,jpg,png',
+        'file' => 'required|unique:images,file|mimes:jpeg,jpg,png',
     );
     // Match input name when validating data to Model's columns
     public $autoHydrateEntityFromInput = true;
     // Hydrates auto-magically whenever validation is called - for updating
     public $forceEntityHydrationFromInput = true;
     
+	// Belongs to User Model
     public function user(){
         $this->belongsTo('User');
     }
